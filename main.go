f5601292b583151a1b4ba09be6d98c5032aaf871
Landan Fagan <landan@landoware.com>
2026-06-20 08:22:54 -0600

    ~ Discord poc

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	Token := ""
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	Token := os.Getenv("TOKEN")
+
 	discord, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("Failed to create discord session, ", err)
@@ -18,6 +26,17 @@ func main() {
 	}
 
 	discord.Identify.Intents = discordgo.IntentsDirectMessages
+
+	initCommand := discordgo.ApplicationCommand{
+		Name:        "initialize",
+		Description: "Set up notifications for farm runs and setup the RuneLite integration.",
+	}
+
+	_, err = discord.ApplicationCommandCreate("1511535737651335199", "", &initCommand)
+	if err != nil {
+		fmt.Println("Failed to create command, ", err)
+		return
+	}
 
 	discord.AddHandler(onboard)
 
@@ -34,20 +53,53 @@ func main() {
 	<-sc
 
 	// Cleanly close down the Discord session.
-	discord.Close()
+	err = discord.Close()
+	if err != nil {
+		fmt.Println("Failed to close connection, ", err)
+		return
+	}
 }
 
-func onboard(s *discordgo.Session, e *discordgo.IntegrationCreate) {
-	fmt.Println("IntegrationCreate")
-	channel, err := s.UserChannelCreate(e.ID)
+func onboard(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	if i.Type != discordgo.InteractionApplicationCommand {
+		return
+	}
+
+	if i.ApplicationCommandData().Name != "initialize" {
+		return
+	}
+
+	var userID string
+	if i.User != nil {
+		userID = i.User.ID
+	} else if i.Member != nil && i.Member.User != nil {
+		userID = i.Member.User.ID
+	} else {
+		fmt.Println("error resolving interaction user")
+		return
+	}
+
+	channel, err := s.UserChannelCreate(userID)
 	if err != nil {
 		fmt.Println("error creating channel:", err)
 		return
 	}
 
 	// Send the plugin setup message
-	_, err = s.ChannelMessageSend(channel.ID, "Put this in the Discord ID field of the plugin: `"+e.ID+"`")
+	_, err = s.ChannelMessageSend(channel.ID, "Put this in the Discord ID field of the plugin: `"+userID+"`")
 	if err != nil {
 		fmt.Println("error sending DM message:", err)
+		return
+	}
+
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "I sent you a DM with setup instructions.",
+			Flags:   discordgo.MessageFlagsEphemeral,
+		},
+	})
+	if err != nil {
+		fmt.Println("error responding to interaction:", err)
 	}
 }
